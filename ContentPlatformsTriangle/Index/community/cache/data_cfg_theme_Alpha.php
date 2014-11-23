fbcaa244e3fb1027333b1cff2471c689dbd64ea4
marsdenisarobot <joseph.clayton.marsden@gmail.com>
2014-11-24 07:05:21 +0800

    Remove phpBB cache files

--- ContentPlatformsTriangle/Index/community/cache/data_cfg_theme_Alpha.php
+++
@@ -1,4 +0,0 @@
-<?php exit; ?>
-1428068845
-187
-a:5:{s:4:"name";s:5:"Alpha";s:9:"copyright";s:54:"&copy; 2013 Lacey Sutton (http://www.dinosaurfact.net)";s:7:"version";s:5:"1.0.1";s:14:"parse_css_file";b:1;s:8:"filetime";i:1390262665;}
--- ContentPlatformsTriangle/Index/community/cache/data_word_censors.php
+++
@@ -1,4 +0,0 @@
-<?php exit; ?>
-1428068893
-201
-a:2:{s:5:"match";a:2:{i:0;s:52:"#(?<![\p{Nd}\p{L}_-])(meatboy)(?![\p{Nd}\p{L}_-])#iu";i:1;s:53:"#(?<![\p{Nd}\p{L}_-])(meat boy)(?![\p{Nd}\p{L}_-])#iu";}s:7:"replace";a:2:{i:0;s:3:"joe";i:1;s:3:"joe";}}
